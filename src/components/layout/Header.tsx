7718d2bd3be39d53e7766fcf7d6d33b1e8d08977
zoeMeng1225 <zoebo1225@gmail.com>
2026-04-27 10:42:37 -0700

    feat: changed back resume url

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -57,7 +57,7 @@ export function Header() {
             </a>
           ))}
           <a
-            href="/FrontendEngineer_ZoeMeng_Resume.pdf"
+            href="https://drive.google.com/file/d/1_vCe5XFfLtr6t5-I989fXLeCFlvzVPS_/view?usp=sharing"
             target="_blank"
             className={cn(
               "text-sm font-medium text-text-primary px-4 py-1.5 rounded-md border border-border",
